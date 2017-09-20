99d062637755c5c52f20e7943e8aafa9a1752280
Ynlwys <Ynlwys@gmail.com>
2017-09-20 11:17:52 +0800

    用于测试python基础语法

---
+++ ynlwys/utiil/tUtil.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    author：Ynlwys
+
+
+功能介绍：
+    用于测试 基本语法
+"""
+
+# import time
+
+# print "hello world";
+
+# print time.strftime('%Y-%m-%d-%H%M%S',time.localtime(time.time()));
