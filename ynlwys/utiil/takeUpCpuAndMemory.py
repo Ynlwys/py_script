e6ac16eb6a7ff7a4e972b4c41b510f2a44ab9f0d
Ynlwys <Ynlwys@gmail.com>
2017-10-13 16:54:03 +0800

    获取判断cpu状态，并且为cpu内存增加消耗的工具

---
+++ ynlwys/utiil/takeUpCpuAndMemory.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*
+# 处理Excel的包
+
+"""
+    author：Ynlwys
+
+    1.用于从检测结果上增加CPU以及内存的利用率。
+    2.在增加利用率的同时不会影响到系统业务的正常运转。
+        1).业务利用率增加则该程序降低
+        2).业务利用率降低则该程序增加
+"""
+
+import psutil
+
+
+# function of Get CPU State
+def getCPUstate(interval=1):
+    return (" CPU: " + str(psutil.cpu_percent(interval)) + "%")
+
+
+def judgmentCPUstate(v):
+    vNum = str(v).split(" ");
+    cpuUsed = float(vNum[2].split("%")[0]);
+
+    if cpuUsed > 70:
+        print "CPU 使用过高"
+        offTakeUpCPU();
+
+    if cpuUsed < 20:
+        print "CPU 使用过低"
+        onTakeUpCPU();
+
+    print vNum[2];
+
+
+def offTakeUpCPU():
+    print "pkill -9 dd"
+
+
+def onTakeUpCPU():
+    print """for i in `seq 1 $(cat /proc/cpuinfo |grep "physical id" |wc -l)`; do dd if=/dev/zero of=/dev/null & done"""
+
+
+if __name__ == '__main__':
+    judgmentCPUstate(getCPUstate());
